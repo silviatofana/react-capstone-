4428fdd183b00963636ee5f23e585e35a3af7c5d
silviatofana <100236377+silviatofana@users.noreply.github.com>
2022-07-25 19:53:23 +0300

    HomePage.test.js

--- src/__tests__/HomePage.test.js
+++ src/__tests__/HomePage.test.js
@@ -1,22 +1,51 @@
-import React from 'react';
-import renderer from 'react-test-renderer';
+import {
+  render, waitFor, screen, act,
+} from '@testing-library/react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import '@testing-library/jest-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
 import store from '../store/configureStore';
-import NavBar from '../components/NavBar';
 import HomePage from '../pages/HomePage';
+import FetchStats from '../store/api';
 
-describe('Render components', () => {
-  test('render NavBar', () => {
-    const tree = renderer
-      .create(<Router><Provider store={store}><NavBar /></Provider></Router>)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+jest.mock('../store/api');
+
+describe('Render HomePAge', () => {
+  beforeEach(() => {
+    FetchStats.mockResolvedValue(
+      [
+        {
+          continent: 'Africa',
+          country: 'Botswana',
+          country_flag: 'https://disease.sh/assets/img/flags/af.png',
+          country_id: 4,
+          population: 40135620,
+          todays_cases: 17,
+          todays_deaths: 0,
+          todays_recovered: 55,
+          total_active: 13320,
+          total_cases: 156812,
+          total_deaths: 7361,
+          total_recovered: 136131,
+          total_tests: 782926,
+        },
+      ],
+    );
   });
-  test('render NavBar', () => {
-    const tree = renderer
-      .create(<Router><Provider store={store}><HomePage /></Provider></Router>)
-      .toJSON();
-    expect(tree).toMatchSnapshot();
+
+  test('Test shows countries', async () => {
+    await act(async () => {
+      render(
+        <Provider store={store}>
+          <Router>
+            <Routes>
+              <Route exact path="/" element={<HomePage />} />
+            </Routes>
+          </Router>
+        </Provider>,
+      );
+    });
+
+    await waitFor(() => expect(screen.getByText('Botswana')).toBeInTheDocument());
   });
 });
